774f49409a16d5768490276959a17606fb813667
wardvisual <wardvisual@gmail.com>
2022-09-13 11:15:46 +0000

    chore: update api

--- src/pages/api/ai-bot.ts
+++ src/pages/api/ai-bot.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { AiBot } from "lib/ai-bot/ai-bot.service";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { AiBot } from 'lib/ai-bot/ai-bot.service';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 const aiBot = new AiBot();
 
@@ -8,7 +8,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const request = await aiBot.createCompletion(req.body.userRequest);
 
     res.status(200).json({ response: request });
