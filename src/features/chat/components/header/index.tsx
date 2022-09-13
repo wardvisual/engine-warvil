1bd5aba2061b8e9143c5a02440d9edee116990c8
wardvisual <wardvisual@gmail.com>
2022-09-13 15:42:40 +0000

    chore: update header component

--- src/features/chat/components/header/index.tsx
+++ src/features/chat/components/header/index.tsx
@@ -1,17 +1,24 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
 import { useState } from 'react';
+
+import warvilProfile from 'public/assets/warvil-profile.svg';
 
 import Style from './style';
 
 const Header: NextPage = (props) => {
   return (
     <Style.Wrapper>
-      <h2>Welcome, Guest!</h2>
-      <hr />
-      <p>
-        <strong>Warvil</strong> is an AI bot that seeks to respond to your
-        queries about programming.
-      </p>
+      <Image src={warvilProfile} alt="Warvil" />
+
+      <div>
+        <h2>Welcome, Guest!</h2>
+        <hr />
+        <p>
+          <strong>Warvil</strong> is an AI bot that seeks to respond to your
+          queries about programming.
+        </p>
+      </div>
     </Style.Wrapper>
   );
 };
--- src/features/chat/components/header/style.ts
+++ src/features/chat/components/header/style.ts
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 
 const Style = {
   Wrapper: styled.div`
-    color: var(--color-grey);
-    background: var(--color-purple);
-    padding: 1em;
-    border-radius: 5px;
+    display: grid;
+    place-items: center;
+    > div {
+      margin-top: 1em;
+      color: var(--color-grey);
+      background: var(--color-purple);
+      padding: 1em;
+      border-radius: 5px;
+    }
   `,
 };
 
