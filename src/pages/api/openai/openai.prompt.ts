3bf8f2e30fe73e213bef957fb4d3af975d71587e
wardvisual <wardvisual@gmail.com>
2022-12-09 23:30:41 +0000

    chore: update bot name

--- src/pages/api/openai/openai.prompt.ts
+++ src/pages/api/openai/openai.prompt.ts
@@ -1,23 +1,6 @@
 import { Promptable } from './openai.types';
-import { Helper } from './openai.helper';
 
 export class Prompt implements Promptable {
-  // private helper: Helper;
-
-  // constructor() {
-  //   this.helper = new Helper();
-  // }
-
-  // public trainPrompt(command: string, request: string): string {
-  //   let response: string = '';
-
-  //   switch (command) {
-  //     case 'BASIC_QUESTION':
-  //       response = this.helper.basicQuestion(request);
-  //   }
-
-  //   return response;
-  // }
   private formatRequest(userRequest: string): string {
     const userQuestion =
       userRequest[0].toLocaleUpperCase() +
@@ -29,13 +12,13 @@ export class Prompt implements Promptable {
   public trainPrompt(command: string, request: string): string {
     return `
         You: What is Programming?
-        Warvil Bot: Computer programming is the process of performing a particular computation, usually by designing and building an executable computer program. Programming involves tasks such as analysis, generating algorithms, profiling algorithms' accuracy and resource consumption, and the implementation of algorithms. 
+        Warvil: Computer programming is the process of performing a particular computation, usually by designing and building an executable computer program. Programming involves tasks such as analysis, generating algorithms, profiling algorithms' accuracy and resource consumption, and the implementation of algorithms. 
         You: What is an array in Javascript?
-        Warvil Bot: In JavaScript, array is a single variable that is used to store different elements. It is often used when we want to store list of elements and access them by a single variable.
+        Warvil: In JavaScript, array is a single variable that is used to store different elements. It is often used when we want to store list of elements and access them by a single variable.
         You: Who is your developer?
-        Warvil Bot: I was developed by Edward Fernandez also known as wardvisual.
+        Warvil: I was developed by Edward Fernandez also known as wardvisual.
         You: ${this.formatRequest(request)}
-        Warvil Bot: 
+        Warvil: 
     `;
   }
 }
