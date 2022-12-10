dbd0b3c12efd9b2e7033d871955f8f643b30706b
wardvisual <wardvisual@gmail.com>
2022-12-10 19:57:49 +0000

    chore: add trainer class to train chats

---
+++ src/pages/api/openai/openai.trainer.ts
@@ -0,0 +1,45 @@
+export class Trainer {
+  public generateCode(): string {
+    return `
+      You: Write a hello world in C#
+      Warvil: using System; 
+      public class HelloWorld { 
+          public static void Main() 
+          { 
+              Console.WriteLine("Hello World!"); 
+          } 
+      } 
+      You: Write a calculator in Java utilizing methods in each 4 basic operation
+      Warvil: public class Calculator { 
+        public static int add(int a, int b) { 
+          return a + b; 
+        } 
+        public static int subtract(int a, int b) { 
+          return a - b; 
+        } 
+        public static int multiply(int a, int b) { 
+          return a * b; 
+        } 
+        public static int divide(int a, int b) { 
+          if (b != 0) {  
+                  return a / b;  
+              } else { 
+                  System.out.println("Can't divide by zero.");  
+                  return 0;  
+              }  
+        } 
+       
+          public static void main(String[] args) {  
+       
+              // Call the functions and print result  
+       
+              System.out.println("Addition: " + add(5, 7));  
+              System.out.println("Subtraction: " + subtract(5, 7));  
+              System.out.println("Multiplication: " + multiply(5, 7));  
+              System.out.println("Division: " + divide(5, 7));  
+       
+          }  
+      } 
+    `;
+  }
+}
