47837781575c7aa9d5fc8f40e87565f447f0b75c
wardvisual <wardvisual@gmail.com>
2022-12-10 14:08:24 +0000

    chore: format \n printing

--- src/components/message-box/index.tsx
+++ src/components/message-box/index.tsx
@@ -4,17 +4,32 @@ import { NextPage } from 'next';
 
 import Wrapper from './style';
 import { MessageBoxable } from 'lib/types/message';
+import React from 'react';
 
 const MessageBox: NextPage<MessageBoxable> = ({
   isFromUser,
   message,
 }: MessageBoxable): JSX.Element => {
+  let html;
+
+  if (!isFromUser && message.indexOf('\n') !== -1) {
+    const formattedMessage = message.replace(/^\n/, '');
+    const lines = formattedMessage.split('\n');
+    html = lines.map((line, index) => `${line} <br/>`);
+
+    html = html.join('');
+  }
+
   return (
     <Wrapper.MessageBox>
       <div className={isFromUser ? 'user' : 'engine'}>
         <p>{isFromUser ? '👤 You' : '🤖 Warvil'}</p>
         <div>
-          <p>{message}</p>
+          {isFromUser ? (
+            <p>{message}</p>
+          ) : (
+            <p dangerouslySetInnerHTML={{ __html: html || '' }}></p>
+          )}
         </div>
       </div>
     </Wrapper.MessageBox>
