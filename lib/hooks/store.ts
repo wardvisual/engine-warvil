5f37c1c15c1019713e8cb9ede980ea055a4efbc8
wardvisual <wardvisual@gmail.com>
2022-12-15 19:05:11 +0000

    refactor: zustand store

--- lib/hooks/store.ts
+++ lib/hooks/store.ts
@@ -4,6 +4,42 @@ import { persist } from 'zustand/middleware';
 import create from 'zustand';
 import { Storeable } from '../types/store';
 import { MessageBoxable } from 'lib/types/message';
+import { commands } from 'lib/constants/commands';
+
+// export const initializeStore = (preloadedState = {}) => {
+//   create(
+//     persist(
+//       (set) => ({
+//         ...preloadedState,
+//         isFromUser: false,
+//         isLoading: false,
+//         message: '',
+//         initialCodeInstruction: true,
+//         userInputRequest: '',
+//         appRequests: [],
+//         setUserInputRequest: (userInputRequest: string) => {
+//           set((state: Storeable) => ({
+//             userInputRequest,
+//             initialCodeInstruction: false,
+//           }));
+//         },
+
+//         setAppRequests: (appRequests: MessageBoxable[]) => {
+//           set((state: Storeable) => ({
+//             appRequests,
+//           }));
+//         },
+
+//         setIsLoading: (isLoading: boolean) => {
+//           set((state: Storeable) => ({
+//             isLoading,
+//           }));
+//         },
+//       }),
+//       { name: 'warvil' }
+//     )
+//   );
+// };
 
 export const useStore = create<any>(
   persist<any>(
@@ -14,6 +50,7 @@ export const useStore = create<any>(
       initialCodeInstruction: true,
       userInputRequest: '',
       appRequests: [],
+      command: commands.GENERATE_CODE,
       setUserInputRequest: (userInputRequest: string) => {
         set((state: Storeable) => ({
           userInputRequest,
