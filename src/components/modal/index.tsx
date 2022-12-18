6a8c41e8aa098b177014bd87b6abf6a33c4c8de1
wardvisual <wardvisual@gmail.com>
2022-12-18 22:04:30 +0000

    fix: return JSX element before dynamic rendering

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { NextPage } from 'next';
 
 import Wrapper from './style';
-import { useState, useEffect } from 'react';
+import { Modalable } from 'lib/types/props';
 
-const Modal: NextPage = (prop: any) => {
+const Modal: NextPage<Modalable> = (prop: Modalable) => {
   return (
-    prop.display && (
-      <div>
-        <Wrapper.Overlay onClick={prop.handleOnClose} />
-        <Wrapper.Modal>
-          <Wrapper.ModalHeader>
-            <h2>Modal Title</h2>
-            <i className="fas fa-times" onClick={prop.handleOnClose}></i>
-          </Wrapper.ModalHeader>
-          <Wrapper.ModalBody>{prop.children}</Wrapper.ModalBody>
-        </Wrapper.Modal>
-      </div>
-    )
+    <div>
+      {prop.display && (
+        <>
+          <Wrapper.Overlay onClick={prop.handleOnClose} />
+          <Wrapper.Modal>
+            <Wrapper.ModalHeader>
+              <h2>Modal Title</h2>
+              <i className="fas fa-times" onClick={prop.handleOnClose}></i>
+            </Wrapper.ModalHeader>
+            <Wrapper.ModalBody>{prop.children}</Wrapper.ModalBody>
+          </Wrapper.Modal>
+        </>
+      )}
+    </div>
   );
 };
 
