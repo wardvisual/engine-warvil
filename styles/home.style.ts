8635aff76272202864da5ec27ea6a2b367e6b6f7
wardvisual <wardvisual@gmail.com>
2022-12-20 13:45:02 +0000

    chore: omit header style and seperate it to other component

--- styles/home.style.ts
+++ styles/home.style.ts
@@ -6,25 +6,6 @@ const Wrapper = {
     margin: 0 auto;
     max-width: 1660px;
   `,
-  Header: styled.header`
-    div {
-      padding: 2em;
-
-      &.message-box {
-        border-bottom: 1px var(--color-black-400) solid;
-      }
-    }
-
-    @media screen and (max-width: 992px) {
-      div {
-        img {
-          max-width: 10em !important;
-        }
-        padding: 0.5em;
-        margin-bottom: 0.8em;
-      }
-    }
-  `,
   Container: styled.div`
     display: flex;
     gap: 1em;
