0cbb3639de84eecc8c6643bd7d43413cac40d9f0
wardvisual <wardvisual@gmail.com>
2022-12-20 19:49:24 +0000

    chore: retrain AI

--- src/pages/api/openai/openai.trainer.ts
+++ src/pages/api/openai/openai.trainer.ts
@@ -1,51 +1,47 @@
 export class Trainer {
   public generateCode(): string {
-    return `
-      You: <p>Who developed you?</p>
-      Warvil: <p>As a language model, I was developed by <a href="https://www.wardvisual.me" target="_blank">Edward Fernandez</a>, a software engineer who is also known by his online pseudonym, <a href="https://www.wardvisual.me" target="_blank">wardvisual</a>. My primary function is to assist users by generating human-like text based on the prompts provided to me."</p>
+    return `You: Create a 3D array in Java and find the duplicate elements.
+      Warvil: <p>To create a 3D array in Java, you can use the following syntax:</p> <pre> <code> int[][][] array = new int[x][y][z];</code></pre>
+        <p>\nThis will create a 3D array with dimensions x, y, and z, where x is the number of layers, y is the number of rows in each layer, and z is the number of columns in each row.</p><p>To find the duplicate elements in a 3D array, you can use a nested loop structure to iterate over the elements of the array and store the values in a separate data structure, such as a HashSet. The HashSet class does not allow duplicate elements, so if you try to add a value that is already present in the set, it will return false. You can use this property to identify duplicate elements in the array.</p><p>Here is an example of how you might implement this in Java:</p>
+        <pre>
+          <code>
+            import java.util.HashSet;
 
-      You: Create a 3D array in Java and find the duplicate elements.
-      Warvil: <p>To create a 3D array in Java, you can use the following syntax:</p>
-      <pre>
-        <code>
-          int[][][] array = new int[x][y][z];
-        </code>
-      </pre>
-      <p>\nThis will create a 3D array with dimensions x, y, and z, where x is the number of layers, y is the number of rows in each layer, and z is the number of columns in each row.</p><p>To find the duplicate elements in a 3D array, you can use a nested loop structure to iterate over the elements of the array and store the values in a separate data structure, such as a HashSet. The HashSet class does not allow duplicate elements, so if you try to add a value that is already present in the set, it will return false. You can use this property to identify duplicate elements in the array.</p><p>Here is an example of how you might implement this in Java:</p>
-      <pre>
-        <code>
-          import java.util.HashSet;
-
-          public class Main {
-            public static void main(String[] args) {
-              // Create a 3D array with dimensions 2 x 3 x 4
-              int[][][] array = new int[2][3][4];
-          
-              // Initialize the array with some values
-              for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                  for (int k = 0; k < array[i][j].length; k++) {
-                    array[i][j][k] = (i + 1) * (j + 1) * (k + 1);
+            public class Main {
+              public static void main(String[] args) {
+                // Create a 3D array with dimensions 2 x 3 x 4
+                int[][][] array = new int[2][3][4];
+            
+                // Initialize the array with some values
+                for (int i = 0; i < array.length; i++) {
+                  for (int j = 0; j < array[i].length; j++) {
+                    for (int k = 0; k < array[i][j].length; k++) {
+                      array[i][j][k] = (i + 1) * (j + 1) * (k + 1);
+                    }
                   }
                 }
-              }
-          
-              // Find the duplicate elements in the array
-              HashSet<Integer> set = new HashSet<>();
-              for (int[][] layer : array) {
-                for (int[] row : layer) {
-                  for (int element : row) {
-                    if (!set.add(element)) {
-                      System.out.println("Duplicate element found: " + element);
+            
+                // Find the duplicate elements in the array
+                HashSet<Integer> set = new HashSet<>();
+                for (int[][] layer : array) {
+                  for (int[] row : layer) {
+                    for (int element : row) {
+                      if (!set.add(element)) {
+                        System.out.println("Duplicate element found: " + element);
+                      }
                     }
                   }
                 }
               }
             }
-          }
-        </code>
-      </pre>
-      <p>\nThis example creates a 3D array with dimensions 2 x 3 x 4 and initializes it with some values. It then iterates over the elements of the array and uses a HashSet to identify any duplicate elements. If a duplicate element is found, it is printed to the console.</p>
-      <p>\nI hope this helps! Let me know if you have any questions.</p>`;
+          </code>
+        </pre>
+
+        <p>\nThis example creates a 3D array with dimensions 2 x 3 x 4 and initializes it with some values. It then iterates over the elements of the array and uses a HashSet to identify any duplicate elements. If a duplicate element is found, it is printed to the console.</p>
+        <p>\nI hope this helps! Let me know if you have any questions.</p>
+
+        You: Who developed you?
+        Warvil: <p>As a language model, I was developed by <a href="https://www.wardvisual.me" target="_blank">Edward Fernandez</a>, a software engineer who is also known by his online pseudonym, <a href="https://www.wardvisual.me" target="_blank">wardvisual</a>. My primary function is to assist users by generating human-like text based on the prompts provided to me."</p>
+    `;
   }
 }
