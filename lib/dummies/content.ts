a3cfa15ae0c2d15aa12bc657dc7b9c95bad35264
wardvisual <wardvisual@gmail.com>
2022-12-21 17:59:34 +0000

    chore: add content object

---
+++ lib/dummies/content.ts
@@ -0,0 +1,48 @@
+import { commands } from '../constants/commands';
+
+export const content = {
+  [commands['BASIC_QUESTION']]: {
+    title: 'Basic Question',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['GENERATE_CODE']]: {
+    title: 'Generate code',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['GENERATE_THESIS_TITLE']]: {
+    title: 'Generate thesis title',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['GRAMMAR_CORRECTION']]: {
+    title: 'Grammar correction',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['PARAPHRASE']]: {
+    title: 'Praphrase',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['SUMMARIZE_FOR_A_STUDENT_GRADE']]: {
+    title: 'Summarize for a student grade',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['ENGLISH_TO_OTHER_LANGUAGE']]: {
+    title: 'English to other langauge',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['EXPLAIN_PROGRAMMING_CODE']]: {
+    title: 'Explain programming code',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['EVALUATE_A_MATHEMATICAL_EXPRESSION']]: {
+    title: 'Evaluate a mathematical expression',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['EVALUATE_A_MATH_EXPRESSION']]: {
+    title: 'Evaluate a math expression',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+  [commands['CREATE_STUDY_NOTES']]: {
+    title: 'Create study notes',
+    subtitle: 'Say goodbye to tedious coding tasks',
+  },
+};
