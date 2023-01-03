51e820393f9bae3714779a6645f0f30166b4f687
wardvisual <wardvisual@gmail.com>
2023-01-03 01:16:04 +0000

    chore: omit loggers

--- lib/api/openai/openai.service.ts
+++ lib/api/openai/openai.service.ts
@@ -151,7 +151,6 @@ class OpenAIClient {
         };
       }
     } catch (error: any) {
-      console.log({ server: error.message });
       const msg = {
         preffered: "I'm sorry, I don't understand your question!",
         expiredToken: `I'm sorry, but the service is currently not available due to development work. Please check back later or contact wardvisual, the developer of this app, for further assistance.`,
