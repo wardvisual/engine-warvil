922e8dc66c232d5b81abdf676201219916ef175b
Edward Fernandez <wardvisual@gmail.com>
2023-02-03 17:00:35 +0800

    chore: enable webpack auto refresh

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 3000,
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
